81bf4a551faa3f1bb1af2784b578baa938949d59
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-02-05 17:35:55 -0300

    adicionando models

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
-def estudantes(request):
+def estudantes(request): #pode ser qualquer palavra chave, mas por padrão utilizamos em ingles 'request'
     if request.method == 'GET':
         estudante = {
             'id':1,
