ef24267d3224b7c93b01aa7c7b86e589be4cd71a
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-02-05 21:28:55 -0300

    Adicionando viewset e url de Matricula

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from escola.models import Estudante,Curso
-from escola.serializers import EstudanteSerializer,CursoSerializer
+from escola.models import Estudante,Curso,Matricula
+from escola.serializers import EstudanteSerializer,CursoSerializer,MatriculaSerializer
 
 class EstudantesViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
@@ -10,3 +10,6 @@ class CursosViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from escola.views import EstudantesViewSet,CursosViewSet
+from escola.views import EstudantesViewSet,CursosViewSet,MatriculaViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('estudantes',EstudantesViewSet,basename='Estudantes')
 router.register('cursos',CursosViewSet,basename='Cursos')
+router.register('matriculas',MatriculaViewSet,basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
