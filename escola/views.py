7668e99f2e4544dc1884d68839f7f41126c98950
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-02-05 22:02:03 -0300

    adicionando autenticacao

--- escola/views.py
+++ escola/views.py
@@ -1,15 +1,19 @@
 from rest_framework import viewsets, generics
 from escola.models import Estudante,Curso,Matricula
 from escola.serializers import EstudanteSerializer,CursoSerializer,MatriculaSerializer,ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class EstudantesViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class CursosViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
-
+    
 class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
